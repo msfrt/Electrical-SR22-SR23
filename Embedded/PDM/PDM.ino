788f615a5aea1c9fb1cf3b5a739960f343fde673
Dave Yonkers <yonkers4@msu.edu>
2022-01-21 19:24:57 -0500

    Add last year's PDM code

---
+++ Embedded/PDM/PDM.ino
@@ -0,0 +1,300 @@
+// Dave Yonkers, 2020
+
+#include <EasyTimer.h>
+#include <PWMControl.h>
+#include <ReadADC.h>
+#include <StateCAN.h>
+#include <FlexCAN_T4.h>
+#include <SPI.h>
+#include <Adafruit_NeoPixel.h>
+#include <BoardTemp.h>
+#include <EepromHelper.h>
+
+/* TODOS:
+ *  -
+*/
+
+
+// global variable definition
+int GLO_engine_state = 0; // engine state (no need to change this variable)
+const int GLO_read_resolution_bits = 12; // bits for Teensy-based read resolution
+const int GLO_max_analog_write_pwm = 255; // maximum PWN value
+
+// minimum voltage for the engine to be in "cranking" mode
+const int GLO_cranking_starter_volt_threshold = 5;
+// max rpm for the engine to still be in "cranking" mode
+const int GLO_cranking_rpm_threshold = 2000;
+// engine running min rpm threshold
+const int GLO_engine_on_rpm_threshold = 900;
+// engine cooldown state duration in milliseconds
+const int GLO_engine_cooldown_duration = 5000;
+
+const int GLO_brakelight_min_pressure_F = 150; // minimum pressure required to activate the brakelight (PSI)
+const int GLO_brakelight_min_pressure_R = 125;
+const int GLO_brakelight_teensy_pin = 4;
+
+const int GLO_data_circuit_teensy_pin = 5;
+
+const int GLO_NeoPixel_teensy_pin = 2;
+      int GLO_NeoPixel_brightness_percent = 10; // 0 - 100 %
+
+// boolean used in the C50 to determine if we should be logging
+bool GLO_data_log_bool = 0;
+const unsigned long data_log_timeout_ms = 5000; // time to log after engine and cooldown mode has stopped
+
+Adafruit_NeoPixel GLO_obd_neopixel(1, GLO_NeoPixel_teensy_pin, NEO_GRB + NEO_KHZ800);
+
+//BoardTemp(int pin, int read_bits, int temp_cal, int mv_cal);
+BoardTempDiode board_temp(21, GLO_read_resolution_bits, 28.1, 594);
+EasyTimer board_temp_sample_timer(50);
+
+// EEPROM
+const int eeprom_cs_pin = 9;
+EEPROM_25LC128 eeprom(eeprom_cs_pin);
+
+// engine time clock update frequency. (can be quite low, but don't set too low, as we want this to still be accurate)
+// the actual time checking happens in the timer function.
+EasyTimer engine_time_update_timer(1);
+
+// odemeter update frequency. Should probably keep this pretty low, because if it updates too fast, the calculations
+// will be so small that mileage may actually never be incremented because of poor floating-point math
+EasyTimer odometer_update_timer(2);
+
+// eeprom-saved signals
+#include "EEPROM_sigs.hpp"
+
+// useful sensor sampling definitions can be found here
+#include "sensors.hpp"
+
+// this file contains all of the control tables & declarations for fans and water pump
+#include "fans.hpp"
+
+// signal definitions are inside
+#include "sigs_inside.hpp"
+
+// CAN message definitions are inside
+#include "can_send.hpp"
+
+// CAN read functions are inside
+#include "can_read.hpp"
+
+// odds and ends functions
+#include "misc_fcns.hpp"
+
+// on-board diagnostics
+#include "obd.hpp"
+
+// timer that you can use to print things out for debugging
+EasyTimer debug(1);
+const bool GLO_debug = false;
+
+
+
+
+void setup() { //high 18 low 26
+
+  analogReadResolution(GLO_read_resolution_bits);
+
+  // begin OBD Neopixel
+  GLO_obd_neopixel.begin();
+  GLO_obd_neopixel.setBrightness(map(GLO_NeoPixel_brightness_percent, 0, 100, 0, 255));
+  GLO_obd_neopixel.setPixelColor(0, 255, 0, 0); // red
+  GLO_obd_neopixel.show();
+
+  // Initialize serial communication
+  Serial.begin(112500);
+
+  //initialize the CAN Bus and set its baud rate to 1Mb
+  cbus1.begin();
+  cbus1.setBaudRate(1000000);
+  cbus2.begin();
+  cbus2.setBaudRate(1000000);
+  set_mailboxes();
+
+  // populate left fan table
+  int *fanl_table_ptr = fan_left_table[0]; // create a temp ptr to populate PWM device
+  fan_left.fill_table(fanl_table_ptr); // populate the PWMDevice table
+
+  // populate right fan table
+  int *fanr_table_ptr = fan_right_table[0];
+  fan_right.fill_table(fanr_table_ptr);
+
+  // populate water pump table
+  int *wp_table_ptr = wp_table[0];
+  water_pump.fill_table(wp_table_ptr);
+
+  // initialize the ADC sensors
+  initialize_ADCs();
+
+  // initialize SPI communication
+  SPI.begin();
+
+  // initialize the data circuit pin
+  pinMode(GLO_data_circuit_teensy_pin, OUTPUT);
+  // turn the data circuit on
+  digitalWrite(GLO_data_circuit_teensy_pin, HIGH);
+  // initialize brakelight pin
+  pinMode(GLO_brakelight_teensy_pin, OUTPUT);
+
+  // EEPROM
+  eeprom.begin();
+
+  // write the eeprom variables that are not commented out in the write eeprom function in the EEPROM_sigs file
+  initialize_eeprom_variables();
+
+  GLO_obd_neopixel.setPixelColor(0, 0, 255, 0); // green
+  GLO_obd_neopixel.show();
+
+  // board temp initialization
+  board_temp.begin();
+
+  // neat brakelight animation
+  brakelight_startup();
+
+}
+
+
+void loop() {
+
+  // sensor sampling
+  sample_ADCs();
+  if (board_temp_sample_timer.isup()){
+    board_temp.sample();
+  }
+
+  // read both can buses
+  read_can1();
+  read_can2();
+
+  // run the brakelight
+  brakelight_run();
+  
+
+  // determine engine state and control PWM outputs
+  determine_engine_state(GLO_engine_state);
+  fan_left.set_pwm(GLO_engine_state);
+  fan_right.set_pwm(GLO_engine_state);
+  water_pump.set_pwm(GLO_engine_state);
+
+  // determine if we need to log right now
+  GLO_data_log_bool = determine_logging_state(GLO_engine_state, GLO_engine_cooldown_duration);
+
+  // continously run OBD (individual timers are included)
+  obd_main();
+
+  // engine timer update
+  if (engine_time_update_timer.isup()){
+    engine_timer(eeprom_engine_hours, eeprom_engine_minutes);
+  }
+
+  // odometer update
+  if (odometer_update_timer.isup())
+    odometer(M400_groundSpeed, eeprom_mileage);
+
+  // send all of the things
+  send_can1();
+  send_can2();
+
+  if (GLO_debug && debug.isup()){
+    Serial.println();
+    Serial.println(eeprom_engine_hours.value());
+    Serial.println(eeprom_engine_minutes.value());
+  }
+}
+
+
+
+void set_mailboxes(){
+
+  // to view mailbox status, you can use the member function mailboxStatus(). Don't put it in a fast loop, though,
+  // because you may actually affect how the chips moves things around
+
+  // CAN 2 - sends a bunch of stuff
+  cbus2.setMaxMB(64); // change from default 16 mailboxes to 64 (maximum)
+  cbus2.enableFIFO(); // first in-first out prevents overwriting unsent messages depending on the queue
+  cbus2.setMB(MB4,RX,STD);  // change the first 12 mailboxes to recieve standard frames. 4 for extended. the first four
+  cbus2.setMB(MB5,RX,STD);  // already do by default. the rest of the mailboxes (48) are TX mailboxes by default, which
+  cbus2.setMB(MB6,RX,STD);  // is necessary because we send more data than the bus can handle in short periods of time
+  cbus2.setMB(MB7,RX,STD);
+  cbus2.setMB(MB8,RX,STD);
+  cbus2.setMB(MB9,RX,STD);
+  cbus2.setMB(MB10,RX,STD);
+  cbus2.setMB(MB11,RX,STD);
+  cbus2.setMB(MB12,RX,EXT);  // perhaps there is an issue and something is sending extended frames for whatever reason
+  cbus2.setMB(MB13,RX,EXT);  // we have a lot of mailboxes anyways, so these four can be set to extended
+  cbus2.setMB(MB14,RX,EXT);
+  cbus2.setMB(MB15,RX,EXT);
+  // cbus2.setMB(MB16,TX);
+  // cbus2.setMB(MB17, TX);
+  // cbus2.setMB(MB18, TX);
+  // cbus2.setMB(MB19, TX);
+  // cbus2.setMB(MB20, TX);
+  // cbus2.setMB(MB21, TX);
+  // cbus2.setMB(MB22, TX);
+  // cbus2.setMB(MB23, TX);
+  // cbus2.setMB(MB24, TX);
+  // cbus2.setMB(MB25, TX);
+  // cbus2.setMB(MB26, TX);
+  // cbus2.setMB(MB27, TX);
+  // cbus2.setMB(MB28, TX);
+  // cbus2.setMB(MB29, TX);
+  // cbus2.setMB(MB30, TX);
+  // cbus2.setMB(MB31, TX);
+  // cbus2.setMB(MB32, TX);
+  // cbus2.setMB(MB33, TX);
+  // cbus2.setMB(MB34, TX);
+  // cbus2.setMB(MB35, TX);
+  // cbus2.setMB(MB36, TX);
+  // cbus2.setMB(MB37, TX);
+  // cbus2.setMB(MB38, TX);
+  // cbus2.setMB(MB39, TX);
+  // cbus2.setMB(MB40, TX);
+  // cbus2.setMB(MB41, TX);
+  // cbus2.setMB(MB42, TX);
+  // cbus2.setMB(MB43, TX);
+  // cbus2.setMB(MB44, TX);
+  // cbus2.setMB(MB45, TX);
+  // cbus2.setMB(MB46, TX);
+  // cbus2.setMB(MB47, TX);
+  // cbus2.setMB(MB48, TX);
+  // cbus2.setMB(MB49, TX);
+  // cbus2.setMB(MB50, TX);
+  // cbus2.setMB(MB51, TX);
+  // cbus2.setMB(MB52, TX);
+  // cbus2.setMB(MB53, TX);
+  // cbus2.setMB(MB54, TX);
+  // cbus2.setMB(MB55, TX);
+  // cbus2.setMB(MB56, TX);
+  // cbus2.setMB(MB57, TX);
+  // cbus2.setMB(MB58, TX);
+  // cbus2.setMB(MB59, TX);
+  // cbus2.setMB(MB60, TX);
+  // cbus2.setMB(MB61, TX);
+  // cbus2.setMB(MB62, TX);
+  // cbus2.setMB(MB63, TX);
+
+
+  cbus1.setMaxMB(64);
+  cbus1.enableFIFO();
+  cbus1.setMB(MB4,RX,STD);  // first 16 mailboxes as rx, 4 rx extended. this is pretty overkill, but hey, here they are
+  cbus1.setMB(MB5,RX,STD);
+  cbus1.setMB(MB6,RX,STD);
+  cbus1.setMB(MB7,RX,STD);
+  cbus1.setMB(MB8,RX,STD);
+  cbus1.setMB(MB9,RX,STD);
+  cbus1.setMB(MB10,RX,STD);
+  cbus1.setMB(MB11,RX,STD);
+  cbus1.setMB(MB12,RX,STD);
+  cbus1.setMB(MB13,RX,STD);
+  cbus1.setMB(MB14,RX,STD);
+  cbus1.setMB(MB15,RX,STD);
+  cbus1.setMB(MB16,RX,EXT);
+  cbus1.setMB(MB17,RX,EXT);
+  cbus1.setMB(MB18,RX,EXT);
+  cbus1.setMB(MB19,RX,EXT);
+}
+
+
+
+
+
---
+++ Embedded/PDM/misc_fcns.hpp
@@ -0,0 +1,185 @@
+#ifndef MISC_FUNCTIONS_HPP
+#define MISC_FUNCTIONS_HPP
+
+
+
+
+template <class T1, class T2>
+void engine_timer(EEPROM_Value<T1> &hours, EEPROM_Value<T2> &minutes){
+  static unsigned int last_minute_millis = 0;
+
+  // if the engine is not on (state 2), keep setting the last minute to the current time. This ensures that the
+  // next minute is only triggered when the engine is actuall on.
+  if (GLO_engine_state != 2){
+    last_minute_millis = millis();
+  }
+
+  if (millis() > last_minute_millis + 60000){  // 60,000ms = 60s
+
+    // minutes AND hours have changed
+    if (eeprom.read(minutes) >= 59){
+      hours = eeprom.read(hours) + 1;
+
+      minutes = 0;
+      eeprom.write(hours);
+      eeprom.write(minutes);
+
+    // just minutes have changed
+    } else {
+      minutes = eeprom.read(minutes) + 1;
+      eeprom.write(minutes);
+    }
+
+    // update the last minute timer
+    last_minute_millis = millis();
+  }
+}
+
+
+
+// call the odemeter at a decently fast rate for semi-accurate calculations. Beware, if you call this too fast, the
+// time difference may be close to 0, and therefore may not. MILEAGE STORED IS FACTORED BY 10
+template <class T1>
+void odometer(StateSignal &speed, EEPROM_Value<T1> &mileage10){
+  static unsigned int last_calc_time = 0;
+  static float calc_period_seconds = 0.0;
+  static float miles10_per_second = 0;
+  static float running_mileage10 = 0; // counts mileage10 since start up. Once it reaches 1, the value in memory is
+                                      // incremented and this is reset to 0
+
+
+  // determine the number of seconds for this calculation
+  calc_period_seconds = (millis() - last_calc_time) / 1000.0;
+  last_calc_time = millis();
+
+  // if the car is moving forward, the speed signal is valid, and the car is determined to be on: increment mileage
+  if (speed.value() > 0 && speed.is_valid() && GLO_engine_state == 2){
+
+    // turn miles per hour to miles10 per second
+    miles10_per_second = (speed.value() / 3600.0) * 10;
+
+    // calulate the mileage10 since the last calculation and add it to the running total
+    running_mileage10 += miles10_per_second * calc_period_seconds;
+
+    // if there's been a half of a mile driven, increment the mileage and write it
+    if (running_mileage10 >= 5.0){
+      mileage10 = mileage10.value() + 1;
+      running_mileage10 -= 5.0; // subtract one mile10
+      eeprom.write(mileage10);
+    }
+  }
+}
+
+
+
+
+
+// this function takes a reference to the engine state variable and returns a reference to the same variable.
+// the engine state will be updated if determined necessary.
+int &determine_engine_state(int &engine_state){
+  static unsigned long cooldown_until_time;
+  static unsigned long current_time;
+
+  current_time = millis(); // update the time
+
+  // determine if engine is cranking
+  if (starter_volt_sens.last_calc_avg() >= GLO_cranking_starter_volt_threshold &&
+      M400_rpm.value() <= GLO_cranking_rpm_threshold){
+    engine_state = 1; // cranking
+
+  // determine if engine is on
+  } else if (M400_rpm.value() >= GLO_engine_on_rpm_threshold){
+    engine_state = 2; // on
+
+  // determine if cooldown or off
+  } else {
+    // was the engine just running? If it was and we made it to the else statement, trigger cooldown mode.
+    // note: this can only be triggered if the engine was determined previously to be on.
+    if (engine_state == 2){
+      cooldown_until_time = current_time + GLO_engine_cooldown_duration;
+    }
+
+    // should we still be in the cooldown? Otherwise the engine is off
+    if (current_time <= cooldown_until_time){
+      engine_state = 3;
+    } else {
+      engine_state = 0;
+    }
+  }
+
+
+  return engine_state;
+}
+
+
+
+
+
+bool determine_logging_state(int &engine_state, const unsigned long &timeout_dur){
+  static unsigned long log_until_time;
+  static bool log_bool = 0;
+
+  if (engine_state == 1 || engine_state == 2 || engine_state == 3){
+    log_until_time = millis() + timeout_dur;
+  }
+
+  // set le bool
+  if (millis() < log_until_time){
+    log_bool = 1;
+  } else {
+    log_bool = 0;
+  }
+
+  return log_bool;
+}
+
+
+
+bool brakelight_run(){
+
+  // user override present
+  if (USER_brakeLightOverride.value() >= 0 && USER_brakeLightOverride.value() <= 100){
+
+    analogWrite(GLO_brakelight_teensy_pin, map(USER_brakeLightOverride.value(), 0, 100, 0, GLO_max_analog_write_pwm));
+    return true;
+
+  // brake-pressure exceeds minimum activation pressure
+  } else if (ATCCF_brakePressureF.value() >= GLO_brakelight_min_pressure_F ||
+      ATCCF_brakePressureR.value() >= GLO_brakelight_min_pressure_R){
+    analogWrite(GLO_brakelight_teensy_pin, 255);
+    return true;
+
+  // turn that shit off :)
+  } else {
+    analogWrite(GLO_brakelight_teensy_pin, 0);
+    return false;
+    
+  }
+
+  return false;
+}
+
+
+
+
+void brakelight_startup(){
+  float i = 0;
+  while (i < 3.14159){
+
+    i += 0.01;
+    delay(10);
+
+    float write_val = sin(i) * 1000;  // returns a value between 0 & 1000
+    int pwm = map(write_val, 0, 1000, 0, 220);
+
+    // write to the LED
+    analogWrite(GLO_brakelight_teensy_pin, pwm);
+
+  }
+
+  analogWrite(GLO_brakelight_teensy_pin, 0);
+
+}
+
+
+#endif
